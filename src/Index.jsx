1302501fdddcd630f375a49bfc9e8ea1a57a4a06
Md. Rafiz Uddin <rafiz001@protonmail.ch>
2025-01-20 23:44:42 +0600

    Changed logo

--- src/Index.jsx
+++ src/Index.jsx
@@ -11,60 +11,54 @@ const Index = () => {
   const [data, setData] = useState(null);
   const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
   const navigate = useNavigate();
-  const [netLoader,setNetLoader] = useState('Sync Now');
+  const [netLoader, setNetLoader] = useState('Sync Now');
   useEffect(() => {
     /*if (once.current) return;
     once.current = true;*/
 
     let config = localStorage.getItem("config");
-    if(!config){
+    if (!config) {
       localStorage.setItem("config", JSON.stringify(defaultConfig()))
     }
 
     let datas = localStorage.getItem("datas")
-    if(datas )
-    {
-       
-        setData(JSON.parse(datas));
+    if (datas) {
+
+      setData(JSON.parse(datas));
     }
-    else
-    {
-      readExcelFromUrl().then((output)=>
-        {
-          if(output!==undefined)
-            {
-              setData(output);
-              localStorage.setItem("datas", JSON.stringify(output));
-              console.log(output);
-            }
-          else
-          {
-            toast.error("Unable to fetch file!");
-            //window.location.reload()
-          }});
+    else {
+      readExcelFromUrl().then((output) => {
+        if (output !== undefined) {
+          setData(output);
+          localStorage.setItem("datas", JSON.stringify(output));
+          console.log(output);
+        }
+        else {
+          toast.error("Unable to fetch file!");
+          //window.location.reload()
+        }
+      });
     }
 
   }, [])
   const checkInternetConnection = async () => {
     try {
       const response = await fetch('https://www.google.com/favicon.ico', {
-        method: 'HEAD', 
-        mode: 'no-cors', 
+        method: 'HEAD',
+        mode: 'no-cors',
       });
-  
+
       return true;
     } catch (error) {
       console.error('No internet connection:', error);
       return false;
     }
   };
-  
-  async function syncNow()
-  {
+
+  async function syncNow() {
     setNetLoader('Pinging')
-    const online =await checkInternetConnection();
-    if(online)
-    {
+    const online = await checkInternetConnection();
+    if (online) {
       localStorage.removeItem("datas");
       window.location.reload();
       location.reload();
@@ -73,61 +67,61 @@ const Index = () => {
     setNetLoader('Sync Now')
   }
   return (<>
-  <ToastContainer />
-  <div className=" flex flex-col mb-10">
-    <div className="flex justify-around bg-teal-900 py-2 text-white fixed left-0 bottom-0 w-full">
-      
-      
-        
-        <NavLink to={`/`} 
-        className={({ isActive, isPending }) =>
-          isActive
-            ? " border-b-2 "
-            : isPending
-            ? "pending"
-            : ""
-        }
-      
+    <ToastContainer />
+    <div className=" flex flex-col mb-10">
+      <div className="flex justify-around bg-teal-900 py-2 text-white fixed left-0 bottom-0 w-full">
+
+
+
+        <NavLink to={`/`}
+          className={({ isActive, isPending }) =>
+            isActive
+              ? " border-b-2 "
+              : isPending
+                ? "pending"
+                : ""
+          }
+
         >Routine</NavLink>
 
 
-        <NavLink to={`full`} 
-        className={({ isActive, isPending }) =>
-          isActive
-            ? " border-b-2 "
-            : isPending
-            ? "pending"
-            : ""
-        }
-      
+        <NavLink to={`full`}
+          className={({ isActive, isPending }) =>
+            isActive
+              ? " border-b-2 "
+              : isPending
+                ? "pending"
+                : ""
+          }
+
         >Full Routine</NavLink>
 
 
 
-        <NavLink to={`config`} 
-        className={({ isActive, isPending }) =>
-          isActive
-            ? " border-b-2 "
-            : isPending
-            ? "pending"
-            : ""
-        }
-      
+        <NavLink to={`config`}
+          className={({ isActive, isPending }) =>
+            isActive
+              ? " border-b-2 "
+              : isPending
+                ? "pending"
+                : ""
+          }
+
         >Config</NavLink>
-      
-    </div>
-    <div className="pb-7 bg-teal-950 px-5 md:px-40 xl:px-72">
-{data && 
-    
-    <div className="my-1 flex justify-between rounded-xl bg-teal-800 p-4 text-center text-black">
-  <div className="p-1 text-white">Synced at {new Date(data.updated).getDate()} {months[new Date(data.updated).getMonth()]} {new Date(data.updated).getHours()}:{new Date(data.updated).getMinutes()}</div>
 
-  <button onClick={()=>syncNow()} className="rounded-xl bg-green-300 px-3 py-1">{netLoader}</button>
-</div>}
-    <Outlet context={[data, setData]}/>
-      
+      </div>
+      <div className="pb-7 bg-teal-950 px-5 md:px-40 xl:px-72">
+        {data &&
 
-    </div>
+          <div className="my-1 flex justify-between rounded-xl bg-teal-800 p-4 text-center text-black">
+            <div className="p-1 text-white">Synced at {new Date(data.updated).getDate()} {months[new Date(data.updated).getMonth()]} {new Date(data.updated).getHours()}:{new Date(data.updated).getMinutes()}</div>
+
+            <button onClick={() => syncNow()} className="rounded-xl bg-green-300 px-3 py-1">{netLoader}</button>
+          </div>}
+        <Outlet context={[data, setData]} />
+        <div class="text-white text-right">Developed by: <a className='underline' href='https://rafiz001.github.io/Portfolio/'>Rafiz</a></div>
+
+      </div>
 
     </div>
 
--- src/pages/Config.jsx
+++ src/pages/Config.jsx
@@ -9,153 +9,148 @@ export default function Config() {
 
   const [currentConfig, setCurrentConfig] = useState(defaultConfig());
   const [data, setData] = useOutletContext();
-  const [selectedClass, setSelectedClass] = useState({semester: '', section: ''});
+  const [selectedClass, setSelectedClass] = useState({ semester: '', section: '' });
   const [section, setSection] = useState(null);
   useEffect(() => {
     if (once.current) return;
     once.current = true;
     //getting config from localstorage
     let config = localStorage.getItem("config");
-    if(!config){
+    if (!config) {
 
       localStorage.setItem("config", JSON.stringify(defaultConfig()))
       navigate(0);
     }
-    else{
+    else {
       setCurrentConfig(JSON.parse(config));
     }
 
     //getting class from localstorage
     let class_ = localStorage.getItem("class");
-    if(class_)
-    {
-        setSelectedClass(JSON.parse(class_));
-        
+    if (class_) {
+      setSelectedClass(JSON.parse(class_));
+
     }
 
 
 
   }, [])
 
-  useEffect(()=>{
-    if(data)sectionGenerator();
-  },[selectedClass,data])
+  useEffect(() => {
+    if (data) sectionGenerator();
+  }, [selectedClass, data])
 
 
-  function saveConfig() 
-  {
-    const temp = 
+  function saveConfig() {
+    const temp =
     {
       "url": document.querySelector("[name='url']").value,
-      "timeRow" : parseInt(document.querySelector("[name='timeRow']").value),
-      "timeColumn" : parseInt(document.querySelector("[name='timeColumn']").value),
-      "sectionColumn" : parseInt(document.querySelector("[name='sectionColumn']").value),
-      "semesterColumn" : parseInt(document.querySelector("[name='semesterColumn']").value),
-  }
-  localStorage.setItem("config", JSON.stringify(temp));
-  toast.success("Configuration saved!",{
-    autoClose: 1000,
-    onClose: () => {navigate(0)}
-  });
+      "timeRow": parseInt(document.querySelector("[name='timeRow']").value),
+      "timeColumn": parseInt(document.querySelector("[name='timeColumn']").value),
+      "sectionColumn": parseInt(document.querySelector("[name='sectionColumn']").value),
+      "semesterColumn": parseInt(document.querySelector("[name='semesterColumn']").value),
+    }
+    localStorage.setItem("config", JSON.stringify(temp));
+    toast.success("Configuration saved!", {
+      autoClose: 1000,
+      onClose: () => { navigate(0) }
+    });
   }
 
-  function saveDefault()
-  {
+  function saveDefault() {
     localStorage.setItem("config", JSON.stringify(defaultConfig()));
     toast.success("Configuration restored to default", {
       autoClose: 1000,
-      onClose: () => {navigate("/")}
+      onClose: () => { navigate("/") }
     });
-    
+
   }
 
-  function sectionGenerator()
-  {
+  function sectionGenerator() {
 
     const semesterr = document.querySelector("[name='semester']").value;
-    if(semesterr != "...") setSection(Object.keys(data.data[0][semesterr]));
-   
+    if (semesterr != "...") setSection(Object.keys(data.data[0][semesterr]));
+
   }
 
-  function saveClass()
-  {
-    const temp = {semester: document.querySelector("[name='semester']").value, section: document.querySelector("[name='section']").value};
+  function saveClass() {
+    const temp = { semester: document.querySelector("[name='semester']").value, section: document.querySelector("[name='section']").value };
     localStorage.setItem("class", JSON.stringify(temp));
     navigate("/");
   }
-  function clearStorage()
-  {
-    
+  function clearStorage() {
+
     localStorage.clear();
     toast.success("Configuration cleared.", {
       autoClose: 1000,
-      onClose: () => {navigate("/")}
+      onClose: () => { navigate("/") }
     });
   }
 
   return (
     <>
-    {/*Design:  https://play.tailwindcss.com/14BHbypVbt */}
-    <div class="my-1 flex  rounded-xl bg-teal-800 p-0 text-center text-white "><a className='p-2 w-full underline text-center' href="https://t.me/sheet_routine">Join Telegram Channel for offline app, updates, bug report</a></div>
-    
-    <div className="mt-5 rounded-t-xl bg-teal-800 p-4 text-center text-white">Choose Your Class</div>
-  <div className="bg-teal-500 p-4">
-    <label
-      >Semester:
+      {/*Design:  https://play.tailwindcss.com/14BHbypVbt */}
+      <div class="my-1 flex  rounded-xl bg-teal-800 p-0 text-center text-white "><a className='p-2 w-full underline text-center' href="https://t.me/sheet_routine">Join Telegram Channel for offline app, updates, bug report</a></div>
 
-      <select onChange={()=>sectionGenerator()} name="semester" className="w-full bg-teal-400 p-2">
-        
-      {data && <option value="...">Select Semester</option>}
-        {data ? Object.keys(data.data[0]).map((value,key)=><>
-          <option key={key} selected={value === selectedClass.semester} value={value}>{value}</option>
-        </>):<option value="...">Loading...</option>}
-      </select> 
-    </label>
+      <div className="mt-5 rounded-t-xl bg-teal-800 p-4 text-center text-white">Choose Your Class</div>
+      <div className="bg-teal-500 p-4">
+        <label
+        >Semester:
 
-    <label
-      >Section:
+          <select onChange={() => sectionGenerator()} name="semester" className="w-full bg-teal-400 p-2">
 
-      <select className="w-full bg-teal-400 p-2" name='section'>
-          {section && section.map((value,key)=><>
-          <option key={key} selected={value === selectedClass.section} value={value}>{value}</option>
-        </>)}
-      </select>
-    </label>
+            {data && <option value="...">Select Semester</option>}
+            {data ? Object.keys(data.data[0]).map((value, key) => <>
+              <option key={key} selected={value === selectedClass.semester} value={value}>{value}</option>
+            </>) : <option value="...">Loading...</option>}
+          </select>
+        </label>
 
-        <div className="mt-4 flex justify-center gap-2"><button className="rounded-xl bg-green-300 px-3 py-1" onClick={()=>saveClass()}>💾 Save</button></div>
-  </div>
+        <label
+        >Section:
 
-  <div className="mt-5 rounded-t-xl bg-teal-800 p-4 text-center text-white">Google Sheet Config</div>
-  <div className="bg-teal-500 p-4">
-    <label
-      >Sheet URL:
-      <textarea className="w-full bg-teal-400 p-2" onChange={(e)=>setCurrentConfig(p=>{return {...p,url: e.target.value}})} value={currentConfig.url} name='url'></textarea>
-    </label>
+          <select className="w-full bg-teal-400 p-2" name='section'>
+            {section && section.map((value, key) => <>
+              <option key={key} selected={value === selectedClass.section} value={value}>{value}</option>
+            </>)}
+          </select>
+        </label>
 
-    <label
-      >Time Row:
-      <input type="number" className="w-full bg-teal-400 p-2" onChange={(e)=>setCurrentConfig(p=>{return {...p,timeRow: e.target.value}})} value={currentConfig.timeRow} name='timeRow'/>
-    </label>
+        <div className="mt-4 flex justify-center gap-2"><button className="rounded-xl bg-green-300 px-3 py-1" onClick={() => saveClass()}>💾 Save</button></div>
+      </div>
 
-    <label
-      >Time Column:
-      <input type="number" className="w-full bg-teal-400 p-2" onChange={(e)=>setCurrentConfig(p=>{return {...p,timeColumn: e.target.value}})} value={currentConfig.timeColumn} name='timeColumn'/>
-    </label>
+      <div className="mt-5 rounded-t-xl bg-teal-800 p-4 text-center text-white">Google Sheet Config</div>
+      <div className="bg-teal-500 p-4">
+        <label
+        >Sheet URL:
+          <textarea className="w-full bg-teal-400 p-2" onChange={(e) => setCurrentConfig(p => { return { ...p, url: e.target.value } })} value={currentConfig.url} name='url'></textarea>
+        </label>
 
-    <label
-      >Section Column:
-      <input type="number" className="w-full bg-teal-400 p-2" onChange={(e)=>setCurrentConfig(p=>{return {...p,sectionColumn: e.target.value}})} value={currentConfig.sectionColumn} name='sectionColumn'/>
-    </label>
+        <label
+        >Time Row:
+          <input type="number" className="w-full bg-teal-400 p-2" onChange={(e) => setCurrentConfig(p => { return { ...p, timeRow: e.target.value } })} value={currentConfig.timeRow} name='timeRow' />
+        </label>
 
-    <label
-      >Semester Column:
-      <input type="number" className="w-full bg-teal-400 p-2" onChange={()=>{}} value={currentConfig.semesterColumn} name='semesterColumn'/>
-    </label>
+        <label
+        >Time Column:
+          <input type="number" className="w-full bg-teal-400 p-2" onChange={(e) => setCurrentConfig(p => { return { ...p, timeColumn: e.target.value } })} value={currentConfig.timeColumn} name='timeColumn' />
+        </label>
 
-    <div className="mt-4 flex justify-center gap-2"><button className="rounded-xl bg-green-300 px-3 py-1" onClick={()=>saveConfig()}>💾 Save</button> <button className="rounded-xl bg-green-300 px-3 py-1" onClick={()=>saveDefault()}>↩️ Default</button></div>
-  </div>
-  <div class="my-1 flex  rounded-xl bg-teal-800 p-0 text-center text-black flex justify-center "><button className="rounded-xl bg-green-300 px-3 py-1" onClick={()=>clearStorage()}>❌ Clear Memory</button></div>
-    
+        <label
+        >Section Column:
+          <input type="number" className="w-full bg-teal-400 p-2" onChange={(e) => setCurrentConfig(p => { return { ...p, sectionColumn: e.target.value } })} value={currentConfig.sectionColumn} name='sectionColumn' />
+        </label>
+
+        <label
+        >Semester Column:
+          <input type="number" className="w-full bg-teal-400 p-2" onChange={() => { }} value={currentConfig.semesterColumn} name='semesterColumn' />
+        </label>
+
+        <div className="mt-4 flex justify-center gap-2"><button className="rounded-xl bg-green-300 px-3 py-1" onClick={() => saveConfig()}>💾 Save</button> <button className="rounded-xl bg-green-300 px-3 py-1" onClick={() => saveDefault()}>↩️ Default</button></div>
+      </div>
+      <div class="my-1 flex  rounded-xl bg-teal-800 p-0 text-center text-black flex justify-center "><button className="rounded-xl bg-green-300 px-3 py-1" onClick={() => clearStorage()}>❌ Clear Memory</button></div>
+
+
     </>
   )
 }
